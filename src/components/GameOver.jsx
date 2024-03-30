0f48336be0e7f059bdcc27a42eb19eb32bc4fd19
Taras Nesterenko <tnestere@gmail.com>
2024-03-30 12:22:53 +0100

    added changes

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -4,7 +4,7 @@ const GameOver = ({ winner, restart }) => {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      {winner && <p>{winner} won !</p>}
+      {winner && <p>{winner.toUpperCase()} won !</p>}
       {!winner && <p>It is a draw!</p>}
       <p>
         <button onClick={restart}>Rematch!</button>
